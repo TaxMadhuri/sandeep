cfb33562a5ed3d3dd4c8a74071a9d16e7717af0e
Taxmanager.Bittu_shah <123371985+Taxmanager-Bittu@users.noreply.github.com>
2023-07-15 15:57:17 +0530

    Project Done

---
+++ js/main.js
@@ -0,0 +1,95 @@
+// Typing animation js
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 15px solid #212529; padding-right: 15px;}";
+    document.body.appendChild(css);
+};
+
+// toggle btn
+$("#toggle").click(function() {
+    $(this).toggleClass("on");
+    $("nav").slideToggle();
+});
+
+// sticy nav
+$(window).scroll(function() {
+    if ($(window).scrollTop() >= 300) {
+        $('header').addClass('fixed-header');
+    } else {
+        $('header').removeClass('fixed-header');
+
+    }
+});
+
+// Testimonials
+$('#testimonials, .blog_section').owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
