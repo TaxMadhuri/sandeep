5b52313d5a32200cfeb8b6b98911349fa361b6ec
Taxmanager-Bittu <taxmanager.bittu@gmail.com>
2023-07-22 11:37:45 +0530

    resposivedone

--- js/main.js
+++ js/main.js
@@ -86,11 +86,10 @@ $('#testimonials, .blog_section').owlCarousel({
             items: 1
         },
         600: {
-            items: 3
+            items: 2
         },
         1000: {
             items: 3
         }
     }
 })
-
